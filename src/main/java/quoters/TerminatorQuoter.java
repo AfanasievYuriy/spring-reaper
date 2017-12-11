a947b84830683985d21575e8c92ac7ae7bc3909c
yuriy <knifeparty4@gmail.com>
2017-12-12 01:38:31 +0300

    added custom BeanPostProcessor + custom annotation

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,6 +1,9 @@
 package quoters;
 
 public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
 
     public void setMessage(String message) {
@@ -8,6 +11,10 @@ public class TerminatorQuoter implements Quoter {
     }
 
     public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(message);
+
+        }
         System.out.println("message = " + message);
     }
 }
